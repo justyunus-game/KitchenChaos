ad266e3801240375b444f034fcbc788f50f011c4
Yunus Ali <justyunus.game@gmail.com>
2024-06-07 22:35:12 +0300

    Initial commit

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    public event EventHandler OnInteractAction;
+    private PlayerInputAction playerInputAction;
+    private void Awake() {
+     playerInputAction = new PlayerInputAction();
+     playerInputAction.Player.Enable();
+
+        playerInputAction.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+        
+        return inputVector;
+    }
+}
